6dd4f62d0c19138fef948f22290f1a590b01ad30
Abhi Nallana <ashek2520@gmail.com>
2020-08-26 19:34:03 +0530

    first commit

---
+++ Day-27.java
@@ -0,0 +1,34 @@
+ static class TestDataEmptyArray {
+        public static int[] get_array() {
+            // complete this function
+            int a[]={};
+            return a;
+        }
+    }
+
+    static class TestDataUniqueValues {
+        public static int[] get_array() {
+            // complete this function
+            int a[] = {1,2,3,4,5};
+            return a;
+        }
+
+        public static int get_expected_result() {
+            // complete this function
+            return 0;
+        }
+    }
+
+    static class TestDataExactlyTwoDifferentMinimums {
+        public static int[] get_array() {
+            // complete this function
+            int a[] ={1,3,1};
+            return a;
+        }
+
+        public static int get_expected_result() {
+            // complete this function
+            return 0;
+        }
+    }
+
